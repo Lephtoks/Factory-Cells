1e30d3c500cce31986271426cd1c37e16a6ef05a
lephtoks <igorkalinin771@gmail.com>
2026-07-18 21:26:18 +0900

    ShopScreen is used now

--- Assets/Scripts/UI/Shop/ShopScreen.cs
+++ Assets/Scripts/UI/Shop/ShopScreen.cs
@@ -1,11 +1,14 @@
 using Core;
+using UnityEngine;
 
 namespace UI.Shop
 {
     public class ShopScreen : MonoSingleton<ShopScreen>
     {
+        public RectTransform rectTransform;
         public override void Awake() {
             base.Awake();
+            rectTransform = GetComponent<RectTransform>();
             gameObject.SetActive(false);
         }
     }
