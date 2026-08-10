423d32730f767f6f9334bfbaf9881173ac8f9aee
lephtoks <igorkalinin771@gmail.com>
2026-08-10 10:09:49 +0900

    Chain block updates

--- Assets/Scripts/Cells/Cell.Block.cs
+++ Assets/Scripts/Cells/Cell.Block.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Cells.Object;
 using Cells.Object.Node;
+using Data;
 using Data.GameManagement;
 using DG.Tweening;
 using Entities;
@@ -33,8 +34,19 @@ namespace Cells
         }
 
         public void BlockUpdate(Block block) {
-            // TODO: Chain updates  
             if (block is IBlockUpdatable blockUpdatable) {
+                Queue<IBlockUpdatable> queue = new();
+                List<IBlockUpdatable> updated = new();
+                queue.Enqueue(blockUpdatable);
+                while (queue.Count > 0) {
+                    var current = queue.Dequeue();
+                    current.BlockUpdate();
+                    updated.Add(current);
+                    if (TryGetObject(((Block) current).Position + Direction.NORTH.ToVector2Int(), out var blockObject1) && blockObject1 is IBlockUpdatable newBlockUpdatable1 && !updated.Contains(newBlockUpdatable1)) queue.Enqueue(newBlockUpdatable1);
+                    if (TryGetObject(((Block) current).Position + Direction.EAST.ToVector2Int(), out var blockObject2) && blockObject2 is IBlockUpdatable newBlockUpdatable2 && !updated.Contains(newBlockUpdatable2)) queue.Enqueue(newBlockUpdatable2);
+                    if (TryGetObject(((Block) current).Position + Direction.SOUTH.ToVector2Int(), out var blockObject3) && blockObject3 is IBlockUpdatable newBlockUpdatable3 && !updated.Contains(newBlockUpdatable3)) queue.Enqueue(newBlockUpdatable3);
+                    if (TryGetObject(((Block) current).Position + Direction.WEST.ToVector2Int(), out var blockObject4) && blockObject4 is IBlockUpdatable newBlockUpdatable4 && !updated.Contains(newBlockUpdatable4)) queue.Enqueue(newBlockUpdatable4);
+                }
                 blockUpdatable.BlockUpdate();
             }
         }
